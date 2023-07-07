8f551a1cd5df7da78448b737344e7b6499388b3a
miruormaechea <mormaecheagraiver@itba.edu.ar>
2023-07-07 13:25:01 -0300

    Arreglada la generacion de txt

--- Clases.py
+++ Clases.py
@@ -6,19 +6,21 @@ import random
 import threading
 from Listas_enlazadas import *
 
-#Usamos listas como variables globales porque las necesitábamos modificar 
-#cada vez que usábamos los métodos del Router: init, activar, desactivar y reiniciar.
+# Usamos listas como variables globales porque las necesitábamos modificar
+# cada vez que usábamos los métodos del Router: init, activar, desactivar y reiniciar.
 
 global listaRouters
 global listaActivos
 global eventosRouters
 
-listaRouters = Lista() #Lista enlazada
-listaActivos = Lista() #Lista enlazada
-eventosRouters = [] #Lista secuencial
+listaRouters = Lista()  # Lista enlazada
+listaActivos = Lista()  # Lista enlazada
+eventosRouters = []  # Lista secuencial
+
 
 def timer(tiempo_espera):
     time.sleep(tiempo_espera)
+
 
 class Router:
     def __init__(self, posicion: int, latencia=0.1, estado="ACTIVO"):
@@ -29,36 +31,36 @@ class Router:
         self.posicion = posicion
         self.estado = estado
         self.latencia = latencia
-        self.cola_paquetes_reenviar = Queue() #usamos Queue porque ya está diseñado para listas LIFO
+        self.cola_paquetes_reenviar = Queue()  # usamos Queue porque ya está diseñado para listas LIFO
         self.cola_paquetes_propios = Queue()
         self.lista_paquetes_recibidos = []
 
-        #Estos contadores son para los gráficos:
+        # Estos contadores son para los gráficos:
         self.contador_paquetes_reenviados = 0
         self.contador_paquetes_enviados = 0
 
-        #Agregamos este nuevo Router al sistema
+        # Agregamos este nuevo Router al sistema
         listaRouters.append(Nodo(self))
         listaActivos.append(Nodo(self))
         listaActivos.ordenar()
 
-        #Guardamos los datos del evento para el CCV
+        # Guardamos los datos del evento para el CCV
         nombre = "ROUTER_" + str(self.posicion)
         fecha_evento = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
         eventosRouters.append((nombre, fecha_evento, "ACTIVO"))
 
     def activar(self):
-        #Para activar el ruter, no tiene que estar activo
+        # Para activar el ruter, no tiene que estar activo
         if self.estado != "ACTIVO":
             global eventosRouters
             global listaActivos
 
-            #Modificamos el estado y lo "Informamos" a la lista de Routers activos
+            # Modificamos el estado y lo "Informamos" a la lista de Routers activos
             self.estado = "ACTIVO"
             listaActivos.append(Nodo(self))
             listaActivos.ordenar()
 
-            #Guardamos los datos del evento para el CCV
+            # Guardamos los datos del evento para el CCV
             nombre = "ROUTER_" + str(self.posicion)
             fecha_evento = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
             eventosRouters.append((nombre, fecha_evento, "ACTIVO"))
@@ -71,29 +73,29 @@ class Router:
         global listaActivos
 
         if listaActivos.buscar_inst(self.posicion, "posicion"):
-            #Actualizamos el estado
+            # Actualizamos el estado
             self.estado = "INACTIVO"
 
-            #Guardamos los datos del evento para el CCV
+            # Guardamos los datos del evento para el CCV
             nombre = "ROUTER_" + str(self.posicion)
             fecha_evento = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
             eventosRouters.append((nombre, fecha_evento, "INACTIVO"))
 
-            #Eliminamos al Router de la lista de Routers activos
+            # Eliminamos al Router de la lista de Routers activos
             listaActivos.pop(self.posicion, "posicion")
         else:
             print("Error, el Router especificado no está activo")
 
-    #Función auxiliar para actualizar el estado del router y guardar el evento
+    # Función auxiliar para actualizar el estado del router y guardar el evento
     def reset(self):
         global listaActivos
         global eventosRouters
-        
-        #Actualizamos el estado y lo informamos al sistema
+
+        # Actualizamos el estado y lo informamos al sistema
         self.estado = "RESET"
         listaActivos.pop(self.posicion, "posicion")
 
-        #Guardamos los datos del evento para el CCV
+        # Guardamos los datos del evento para el CCV
         nombre = "ROUTER_" + str(self.posicion)
         fecha_evento = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
         eventosRouters.append((nombre, fecha_evento, "EN_RESET"))
@@ -101,38 +103,40 @@ class Router:
     def reiniciar(self):
         global listaActivos
 
-        #Buscamos al router en la lista activos, si existe entra al if
+        # Buscamos al router en la lista activos, si existe entra al if
         if listaActivos.buscar_inst(self.posicion, "posicion"):
-            #Creamos los threads que llaman a la función de reset 
+            # Creamos los threads que llaman a la función de reset
             # y al timer que controla un tiempo de reseteo aleatorio entre 5 y 10 seg
             threadReset = threading.Thread(target=self.reset, args=())
             threadTiempo = threading.Thread(target=timer, args=(random.randint(5, 10),))
-            
-            #Activamos los threads
+
+            # Activamos los threads
             threadReset.start()
             threadTiempo.start()
 
-            threadTiempo.join() #Esperamos a que termine de pasar el tiempo de reset
+            threadTiempo.join()  # Esperamos a que termine de pasar el tiempo de reset
 
-            #Volvemos a activar el router 
+            # Volvemos a activar el router
             self.activar()
         else:
             print("Error, el Router especificado no está activo")
+
 
 class Paquete:
     def __init__(self, mensaje: str, router_origen: Router, router_destino: Router):
         self.mensaje = mensaje
 
         # Guardamos el objeto Router en cada caso
-        self.router_origen = router_origen  
-        self.router_destino = router_destino 
+        self.router_origen = router_origen
+        self.router_destino = router_destino
         self.router_actual = router_origen
 
-        #Guardamos la hora del evento para el txt
+        # Guardamos la hora del evento para el txt
         self.hora_creacion = datetime.datetime.now().time()
 
-        #Agregamos el paquete a la cola del Router de origen indicado
+        # Agregamos el paquete a la cola del Router de origen indicado
         router_origen.cola_paquetes_propios.put(self)
+
 
 class routingSim:
     def __init__(self, duracion):
@@ -141,13 +145,15 @@ class routingSim:
 
     def routers_txt(listaRouters):
         nodo_actual = listaRouters.head
-        while nodo_actual is not None:
+        routers_sin_mensajes = []
 
+        while nodo_actual is not None:
             router = nodo_actual.dato
-            with open("router_"+str(router.posicion), "a") as archivo:
-                if len(router.lista_paquetes_recibidos)==0:
-                    archivo.write("Este router no ha recibido mensajes")
-                else:
+
+            if len(router.lista_paquetes_recibidos) == 0:
+                routers_sin_mensajes.append(router)
+            else:
+                with open("router_" + str(router.posicion), "w") as archivo:
                     sublistas = {}
 
                     for paquete in router.lista_paquetes_recibidos:
@@ -157,88 +163,87 @@ class routingSim:
                         sublistas[posicion].append(paquete.mensaje)
 
                     for posicion, sublist in sublistas.items():
-                        archivo.write("Origen: Router "+str(posicion)+"\n\n")
+                        archivo.write("Origen: Router " + str(posicion) + "\n")
                         for mensaje in sublist:
-                            archivo.write(mensaje+"\n")
+                            archivo.write(mensaje + "\n")
+            nodo_actual = nodo_actual.prox
 
-            nodo_actual=nodo_actual.prox   
-
-
-                    
-        
+        for router_sin_mensajes in routers_sin_mensajes:
+            with open("router_" + str(router_sin_mensajes.posicion), "w") as archivo:
+                archivo.write("Este router no ha recibido mensajes\n")
 
     def enviar_paquetes(self, paquete: Paquete, lista_activos: Lista, contador=0):
-        #Chequeamos si el mensaje va hacia la izquierda o va hacia la derecha 
+        # Chequeamos si el mensaje va hacia la izquierda o va hacia la derecha
         # viendo las posiciones de origen y destino
-        if paquete.router_actual.posicion < paquete.router_destino.posicion:  #Va para la derecha
+        if paquete.router_actual.posicion < paquete.router_destino.posicion:  # Va para la derecha
 
             if contador != 0:
-                paquete.router_actual.cola_paquetes_reenviar.get()   #Este get solo se usa para eliminar al primero de la queue
+                paquete.router_actual.cola_paquetes_reenviar.get()  # Este get solo se usa para eliminar al primero de la queue
                 paquete.router_actual.contador_paquetes_reenviados += 1
 
             else:
-                #Esto pasa si ya no quedan paquetes para reenviar
-                paquete.router_actual.cola_paquetes_propios.get()    #Este get solo se usa para eliminar el primero de la queue
-                #Anotamos que mandamos un paquete más
+                # Esto pasa si ya no quedan paquetes para reenviar
+                paquete.router_actual.cola_paquetes_propios.get()  # Este get solo se usa para eliminar el primero de la queue
+                # Anotamos que mandamos un paquete más
                 paquete.router_actual.contador_paquetes_enviados += 1
 
-            #Buscamos el próximo Router
+            # Buscamos el próximo Router
             paquete.router_actual = lista_activos.buscar_inst(paquete.router_actual.posicion, "posicion").prox.dato
 
-            #Agregamos el paquete a la cola reenviar del proximo
-            paquete.router_actual.cola_paquetes_reenviar.put(paquete)  
-            contador += 1
-
-            self.enviar_paquetes(paquete, lista_activos, contador)  #Llamada recursiva con router_actual actualizado
-            
-            return None
-
-        elif paquete.router_actual.posicion > paquete.router_destino.posicion: #Va para la izquierda
-            
-            if contador != 0:
-                paquete.router_actual.contador_paquetes_reenviados += 1
-                paquete.router_actual.cola_paquetes_reenviar.get()  
-                
-            else:
-                paquete.router_actual.cola_paquetes_propios.get()  
-                paquete.router_actual.contador_paquetes_enviados += 1
-
-            #Buscamos el próximo Router    
-            paquete.router_actual = lista_activos.buscar_inst_anterior(paquete.router_actual.posicion, "posicion").dato  #Encuentra el router anterior
-            
-            #Agregamos el paquete a la cola reenviar del anterior
+            # Agregamos el paquete a la cola reenviar del proximo
             paquete.router_actual.cola_paquetes_reenviar.put(paquete)
             contador += 1
-            
-            self.enviar_paquetes(paquete, lista_activos, contador)   #Llamada recursiva con router_actual actualizado
-            
+
+            self.enviar_paquetes(paquete, lista_activos, contador)  # Llamada recursiva con router_actual actualizado
+
             return None
 
-        elif paquete.router_actual.posicion == paquete.router_destino.posicion: #Encuentra el router de destino
-            
+        elif paquete.router_actual.posicion > paquete.router_destino.posicion:  # Va para la izquierda
+
             if contador != 0:
-                #Lo eliminamos de la cola reenviar
-                paquete.router_actual.cola_paquetes_reenviar.get()  
+                paquete.router_actual.contador_paquetes_reenviados += 1
+                paquete.router_actual.cola_paquetes_reenviar.get()
+
+            else:
+                paquete.router_actual.cola_paquetes_propios.get()
+                paquete.router_actual.contador_paquetes_enviados += 1
+
+            # Buscamos el próximo Router
+            paquete.router_actual = lista_activos.buscar_inst_anterior(paquete.router_actual.posicion,
+                                                                       "posicion").dato  # Encuentra el router anterior
+
+            # Agregamos el paquete a la cola reenviar del anterior
+            paquete.router_actual.cola_paquetes_reenviar.put(paquete)
+            contador += 1
+
+            self.enviar_paquetes(paquete, lista_activos, contador)  # Llamada recursiva con router_actual actualizado
+
+            return None
+
+        elif paquete.router_actual.posicion == paquete.router_destino.posicion:  # Encuentra el router de destino
+
+            if contador != 0:
+                # Lo eliminamos de la cola reenviar
+                paquete.router_actual.cola_paquetes_reenviar.get()
 
             else:
                 paquete.router_actual.cola_paquetes_propios.get()
 
-            #Lo sumamos a la lista de paquetes recibidos
-            paquete.router_actual.lista_paquetes_recibidos.append(paquete) 
-            paquete.router_actual.lista_paquetes_recibidos=sorted(paquete.router_actual.lista_paquetes_recibidos, key=lambda x: x.router_origen.posicion)
+            # Lo sumamos a la lista de paquetes recibidos
+            paquete.router_actual.lista_paquetes_recibidos.append(paquete)
+            paquete.router_actual.lista_paquetes_recibidos = sorted(paquete.router_actual.lista_paquetes_recibidos,
+                                                                    key=lambda x: x.router_origen.posicion)
             return None
 
     def crear_csv(self):
-        #Abrimos el archivo en modo de escritura
+        # Abrimos el archivo en modo de escritura
         with open('system_log.csv', 'w', newline='') as archivo_csv:
-            
-            #Guardamos el archivo en una variable
+            # Guardamos el archivo en una variable
             writer = csv.writer(archivo_csv)
 
-            #Vamos iterando la lista tomando el número de Router, la fecha y el tipo de evento guardado
+            # Vamos iterando la lista tomando el número de Router, la fecha y el tipo de evento guardado
             for router, fecha_evento, evento in eventosRouters:
-
-                #Escribimos por línea como se pide en las consignas
+                # Escribimos por línea como se pide en las consignas
                 writer.writerow([router, fecha_evento, evento])
 
         print("Se han guardado los eventos en el archivo CSV.")
--- Main.py
+++ Main.py
@@ -4,105 +4,99 @@ from Listas_enlazadas import *
 from Funciones_auxiliares import validarNum, graficar
 import os
 
-#Función simular: solo recibe la duración de la misma, en segundos.
+
+# Función simular: solo recibe la duración de la misma, en segundos.
 def simular(duracion):
-    #Llamado a variables globales (si las llamábamos más abajo, se rompía el código)
+    # Llamado a variables globales (si las llamábamos más abajo, se rompía el código)
     global listaRouters
     global listaActivos
     global eventosRouters
 
-    #Creamos los Routers
-    Router_1=Router(1)
-    Router_2=Router(2)
-    Router_3=Router(3)
-    Router_4=Router(4)
-    Router_5=Router(5)
-    Router_6=Router(6)
+    # Creamos los Routers
 
-    #Creamos el objeto simulación y algunos paquetes para mandar
+    Router_1 = Router(1)
+    Router_2 = Router(2)
+    Router_3 = Router(3)
+    Router_4 = Router(4)
+    Router_5 = Router(5)
+    Router_6 = Router(6)
+
+    # Creamos el objeto simulación y algunos paquetes para mandar
     simulacion = routingSim(duracion)
-    """paquete1 = Paquete("nINFAAAAA", Router_5, Router_1)
-    paquete2 = Paquete("nINFAAAAA", Router_3, Router_5)"""
-    paquete3 = Paquete("nINFAAAAA 1 ", Router_1, Router_5)
-    paquete4 = Paquete("nINFAAAAA 1 del 2", Router_2, Router_5)
-    paquete5 = Paquete("nINFAAAAA 1 del 3", Router_3, Router_5)
-    paquete6 = Paquete("nINFAAAAA 2 del 2", Router_2, Router_5)
-    paquete7 = Paquete("nINFAAAAA 3 del 2", Router_2, Router_5)
-    paquete8 = Paquete("nINFAAAAA 2 del 3", Router_3, Router_5)
+    paquete1 = Paquete("Texto del paquete 1", Router_3, Router_5)
+    paquete2 = Paquete("Texto del paquete 2", Router_3, Router_5)
+    paquete3 = Paquete("Texto del paquete 3 ", Router_1, Router_2)
+    paquete4 = Paquete("Texto del paquete 4", Router_2, Router_3)
 
-    #Enviamos los paquetes creados
-    """simulacion.enviar_paquetes(paquete1, listaActivos)
-    simulacion.enviar_paquetes(paquete2, listaActivos)"""
+    # Enviamos los paquetes creados
+    simulacion.enviar_paquetes(paquete1, listaActivos)
+    simulacion.enviar_paquetes(paquete2, listaActivos)
     simulacion.enviar_paquetes(paquete3, listaActivos)
     simulacion.enviar_paquetes(paquete4, listaActivos)
-    simulacion.enviar_paquetes(paquete5, listaActivos)
-    simulacion.enviar_paquetes(paquete6, listaActivos)
-    simulacion.enviar_paquetes(paquete7, listaActivos)
-    simulacion.enviar_paquetes(paquete8, listaActivos)
 
     try:
-        #Desactivamos un Router
+        # Desactivamos un Router
         listaRouters.buscar_inst(2, "posicion").dato.desactivar()
-        #Router.desactivar(listaRouters.buscar_inst(1, "posicion").dato)
+        # Router.desactivar(listaRouters.buscar_inst(1, "posicion").dato)
     except IndexError:
         print("Error. El router especificado no existe.")
 
     try:
-        #Reiniciamos un Router
+        # Reiniciamos un Router
         listaRouters.buscar_inst(1, "posicion").dato.reiniciar()
-        #Router.reiniciar(listaRouters.buscar_inst(1, "posicion").dato)
+        # Router.reiniciar(listaRouters.buscar_inst(1, "posicion").dato)
     except IndexError:
-        #Puede pasar que se le de como parámetro un Router inexistente 
+        # Puede pasar que se le de como parámetro un Router inexistente
         # (como trabajamos con una lista, se estaría dando un índice erróneo que rompe el programa)
         print("Error. El router especificado no existe.")
 
     try:
-        #Desactivamos un Router
+        # Desactivamos un Router
         listaRouters.buscar_inst(6, "posicion").dato.desactivar()
-        #Router.desactivar(listaRouters.buscar_inst(1, "posicion").dato)
+        # Router.desactivar(listaRouters.buscar_inst(1, "posicion").dato)
     except IndexError:
-        print("Error. El router especificado no existe.")        
-    
+        print("Error. El router especificado no existe.")
+
     try:
-        #Volvemos a activar el Router desactivado
+        # Volvemos a activar el Router desactivado
         listaRouters.buscar_inst(2, "posicion").dato.activar()
     except IndexError:
         print("Error. El router especificado no existe.")
 
+
 def timer(tiempo_espera):
     time.sleep(tiempo_espera)
+
 
 def main():
     global listaRouters
     global listaActivos
     global eventosRouters
 
-    #Pedimos un tiempo de simulación al usuario, validando que sea un número entre 0 y 100000000
-    tiempo_simulacion = validarNum(0, 100000000)
+    # Pedimos un tiempo de simulación al usuario, validando que sea un número entre 0 y 1000
+    tiempo_simulacion = validarNum(0, 1000)
 
-    #Creamos los Threads de simulación y el del timer que limitará a la simulación
+    # Creamos los Threads de simulación y el del timer que limitará a la simulación
     t1 = threading.Thread(target=timer, args=(tiempo_simulacion,))
     t2 = threading.Thread(target=simular, args=(tiempo_simulacion,))
 
-    #Ejecutamos las 2 funciones a través de los Threads
+    # Ejecutamos las 2 funciones a través de los Threads
     t1.start()
     t2.start()
+    routingSim.crear_csv(eventosRouters)  # Escribimos el system_log
+    routingSim.routers_txt(listaRouters) #Escribimos los mensajes recibidos en txt
+    graficar(listaRouters)  # Graficamos los eventos de cada Router
 
-    #Esperamos a que pase el tiempo para dar por terminada la simulación
+    # Esperamos a que pase el tiempo para dar por terminada la simulación
     t1.join()
 
     print("Fin del programa")
-
-    #prueba para ver el orden de los mensajes en la lista de paqutes recibidos
-    for paquete in listaRouters.buscar_inst(5, "posicion").dato.lista_paquetes_recibidos:
-        print (paquete.mensaje)
-        
-    routingSim.crear_csv(eventosRouters) #Escribimos el system_log
-    routingSim.routers_txt(listaRouters)
-    graficar(listaRouters) #Graficamos los eventos de cada Router
-
-    #Terminamos la ejecución del programa
+    # Terminamos la ejecución del programa
     os._exit(0)
+
+    # prueba para ver el orden de los mensajes en la lista de paqutes recibidos
+    for paquete in listaRouters.buscar_inst(5, "posicion").dato.lista_paquetes_recibidos:
+        print(paquete.mensaje)
 
 
 main()
